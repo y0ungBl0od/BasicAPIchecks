92dd24779607b5c6c9b188d9415400b3e7f593a9
y0ungBl0od <anyone-also@bk.ru>
2023-10-06 19:58:51 +0300

    token mutation response test

--- src/test/java/BebsTest.java
+++ src/test/java/BebsTest.java
@@ -9,17 +9,17 @@ public class BebsTest {
 
     public static void main(String[] args) {
 
-        String response = given()
-                .header("Content-type", "Application/json")
-                .header("Authorization", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoielp6cDZ6UHBocE51RVZ1N2FmZHhDaHFmcGpyMSJ9.uuVnWh36PlmsTTNRykldWikw1Qq9i0R66uumDBFF7Ls")
-                .body("{\"operationName\":null,\"variables\":{},\"query\":\"{\\n  profiles(\\n    where: {isCardHolder: false, genderFilter: [NONBINARY], distance: 80000, birthday: [18, 57]}\\n    paginate: {limit: 1000}\\n  ) {\\n    id\\n    name\\n    gender\\n  }\\n}\\n\"}")
-                .when().post("http://217.107.219.153:3000/graphql")
+        int userId = 50774;
+        // token
+        String response =  given().log().all().header("Content-Type","application/json")
+        .body("{\"operationName\":null,\"variables\":{},\"query\":\"mutation {\\n  token(id: "+userId+")\\n}\\n\"}")
+                .when().post("https://api.bebs.app/graphql")
                 .then().extract().response().asString();
         System.out.println(response);
 
-        JsonPath js = new JsonPath(response);
-        String x = js.getString("data.profiles.id[0]");
-        Assert.assertEquals(x, "25");
+        JsonPath token = new JsonPath(response);
+        String tokenValue = token.getString("data.token");
+        Assert.assertEquals(tokenValue,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiM2lnRmUwUHRnMmR5SGtyWlBZV0tGRFB6UkMwMyJ9.mkmqXZ4BB6UKY3opb2ounr5mAB8P4WBrZpf-ylKp_vI");
     }
 
 }
